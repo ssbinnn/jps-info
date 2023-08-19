dd3c21b8eb60b1699bd7c80d49d3320ba8f1378c
ssbinnn <odqls53@gmail.com>
2023-08-19 16:11:08 +0900

    commit

---
+++ app/src/main/java/com/example/jps/BasicJob.java
@@ -0,0 +1,185 @@
+package com.example.jps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import android.os.AsyncTask;
+import android.widget.CheckBox;
+import android.database.Cursor;
+
+
+public class BasicJob extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private JobAdapter jobAdapter;
+    private DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.basic_job);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        dbHelper = new DatabaseHelper(BasicJob.this);
+
+        // Start AsyncTask to read CSV file
+        new ReadCSVFileTask().execute("장애인구직정보_전처리.csv");
+    }
+
+    private class ReadCSVFileTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... filenames) {
+            List<String[]> allData = new ArrayList<>();
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open(filenames[0]), "UTF-8");
+                CSVReader csvReader = new CSVReader(inputStreamReader);
+
+                // 헤더 행 스킵
+                csvReader.readNext();
+
+                String[] nextLine;
+                while ((nextLine = csvReader.readNext()) != null) {
+                    allData.add(nextLine);
+                }
+                csvReader.close();
+            } catch (IOException | CsvValidationException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Cursor cursor = dbHelper.getAllJobs();
+            jobAdapter = new JobAdapter(cursor);
+            recyclerView.setAdapter(jobAdapter);
+        }
+    }
+
+    class JobViewHolder extends RecyclerView.ViewHolder {
+        TextView textView1, textView2, textView3, textView4, textView5, textView6;
+        CheckBox checkBox;
+        Button detailButton;
+        JobViewHolder(View itemView) {
+            super(itemView);
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+            textView3 = itemView.findViewById(R.id.textView3);
+            textView4 = itemView.findViewById(R.id.textView4);
+            textView5 = itemView.findViewById(R.id.textView5);
+            textView6 = itemView.findViewById(R.id.textView6);
+            checkBox = itemView.findViewById(R.id.checkBox);
+            detailButton = itemView.findViewById(R.id.detailButton);
+        }
+    }
+    public static class JobData implements Serializable {
+        private String[] data;
+        private boolean isChecked;
+
+        // static 변수로 체크된 아이템들을 저장할 리스트 추가
+        public static List<JobData> checkedItems = new ArrayList<>();
+
+        public JobData(String[] data) {
+            this.data = data;
+        }
+
+        public String[] getData() {
+            return data;
+        }
+
+        public boolean isChecked() {
+            return isChecked;
+        }
+
+        public void setChecked(boolean checked) {
+            if(checked && !this.isChecked) {
+                // 아이템을 체크 리스트에 추가
+                checkedItems.add(this);
+            } else if (!checked && this.isChecked) {
+                // 아이템을 체크 리스트에서 삭제
+                checkedItems.remove(this);
+            }
+            this.isChecked = checked;
+        }
+
+        // static 메서드로 체크된 아이템 리스트를 얻는 메서드 추가
+        public static List<JobData> getCheckedItems() {
+            return checkedItems;
+        }
+    }
+
+    class JobAdapter extends RecyclerView.Adapter<JobViewHolder> {
+        private List<JobData> jobDataList;
+
+        JobAdapter(Cursor cursor) {
+            this.jobDataList = new ArrayList<>();
+            while(cursor.moveToNext()) {
+                String companyName = getColumnValue(cursor, "company_name");
+                String jobType = getColumnValue(cursor, "job_type");
+                String company_contract = getColumnValue(cursor, "contract");
+                String company_address = getColumnValue(cursor, "Address");
+                String company_StartDay = getColumnValue(cursor, "Start_Day");
+                String company_EndDay = getColumnValue(cursor, "End_Day");
+
+                if(companyName != null && jobType != null && company_contract != null && company_address != null && company_StartDay != null && company_EndDay != null) {
+                    String[] data = new String[] { companyName, jobType, company_contract, company_address, company_StartDay, company_EndDay };
+                    this.jobDataList.add(new JobData(data));
+                }
+            }
+        }
+        private String getColumnValue(Cursor cursor, String columnName) {
+            int index = cursor.getColumnIndex(columnName);
+            if(index != -1) {
+                return cursor.getString(index);
+            }
+            return null;
+        }
+        public JobViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+            return new JobViewHolder(view);
+        }
+
+        public void onBindViewHolder(JobViewHolder holder, int position) {
+            JobData job = jobDataList.get(position);
+
+            holder.textView1.setText("회사명 : " + job.getData()[0]);
+            holder.textView2.setText("직종 업무 : " + job.getData()[1]);
+            holder.textView3.setText("계약 구분 : " + job.getData()[2]);
+            holder.textView4.setText("주소 : " + job.getData()[3]);
+            holder.textView5.setText("시작일자 : " + job.getData()[4]);
+            holder.textView6.setText("마감일자 : " + job.getData()[5]);
+            holder.checkBox.setChecked(job.isChecked());
+            holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> job.setChecked(isChecked));
+
+            // 버튼 설정
+            holder.detailButton.setOnClickListener(v -> {
+                Intent intent = new Intent(v.getContext(), DetailInfo.class);
+                intent.putExtra("jobData", job); // job
+                v.getContext().startActivity(intent);
+            });
+        }
+
+        public int getItemCount() {
+            return jobDataList.size();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jps/DetailInfo.java
@@ -0,0 +1,71 @@
+package com.example.jps;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jps.BasicJob.JobData;
+
+public class DetailInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_info);
+
+        // 이전 액티비티로부터 데이터 가져오기
+        JobData jobdata = (JobData) getIntent().getSerializableExtra("jobData");
+
+        // 기업 이름
+        TextView nameTextView = findViewById(R.id.text_name);
+        nameTextView.setText(jobdata.getData()[1]);
+
+        // 직종, 계약구분, 임금형태+임금, 입사형태
+        TextView TextView_1 = findViewById(R.id.textView_1);
+        TextView_1.setText("직종 : " + jobdata.getData()[2]
+                + "\n" + "계약 구분 : " + jobdata.getData()[3]
+                + "\n" + "임금 : " + jobdata.getData()[4] + " " + jobdata.getData()[5]
+                + "\n" + "입사 형태 : " + jobdata.getData()[6]);
+
+        /*
+        구인신청일자,사업장명,모집직종,고용형태,임금형태,임금,입사형태,
+        요구경력,요구학력,전공계열,요구자격증,사업장 주소,기업형태,담당기관,
+        등록일,연락처,인증구분,등급,인증발급일,인증만료일,시작일자,종료일자,시군구명
+         */
+
+        // 주소
+        TextView TextView_addr = findViewById(R.id.textView_addr);
+        TextView_addr.setText("주소 : " + jobdata.getData()[11]);
+
+        TextView TextView_type = findViewById(R.id.textView_type);
+        TextView_type.setText("기업 형태 : " + jobdata.getData()[12]);
+
+        TextView TextView_phone = findViewById(R.id.textView_phone);
+        TextView_phone.setText("연락처 : " + jobdata.getData()[15]);
+
+        TextView TextView_carrer = findViewById(R.id.textView_carrer);
+        TextView_carrer.setText("요구 경력 : " + jobdata.getData()[7]);
+
+        TextView TextView_edu = findViewById(R.id.textView_edu);
+        TextView_edu.setText("요구 학력 : " + jobdata.getData()[8]);
+
+        TextView TextView_major = findViewById(R.id.textView_major);
+        TextView_major.setText("요구 전공 : " + jobdata.getData()[9]);
+
+        TextView TextView_lisence = findViewById(R.id.textView_lisence);
+        TextView_lisence.setText("요구 자격증 : " + jobdata.getData()[10]);
+
+        TextView TextView_dam = findViewById(R.id.textView_damdang);
+        TextView_dam.setText("담당 기관 : " + jobdata.getData()[13]);
+
+        TextView TextView_in = findViewById(R.id.textView_in);
+        TextView_in.setText("인증 구분 : " + jobdata.getData()[16]);
+
+        TextView TextView_rate = findViewById(R.id.textView_rate);
+        TextView_rate.setText("등급 : " + jobdata.getData()[17]);
+
+        TextView TextView_startend = findViewById(R.id.textView_startend);
+        TextView_startend.setText("시작~종료 일자 : " + jobdata.getData()[20] + " ~ " + jobdata.getData()[21]);
+    }
+}
